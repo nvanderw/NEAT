ad8849b3898789045fbc439dd8b0e5fe4d9fd71c
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-03-25 00:14:09 -0600

    Add test for genome expression

--- Tests.hs
+++ Tests.hs
@@ -1,6 +1,7 @@
 module Tests (testMain) where
 
 import Test.HUnit 
+import Control.Monad
 import Control.Monad.ST
 import Control.Arrow ((>>>))
 import Data.Maybe (fromJust)
@@ -13,7 +14,7 @@ import qualified NEAT.Gene as Gene
 import qualified NEAT.Express as Xpr
 
 -- |Tests that a gene is expressed correctly
-testExpression = TestCase $ do
+testSimpleExpress = TestCase $ do
     org <- stToIO $ Xpr.expressNodeGene (Gene.NodeGene 0) Map.empty
     let (Just neuron) = Map.lookup 0 org
     assertEqual "Error in node gene expression" 0 (Xpr.getNeurID neuron)
@@ -34,6 +35,23 @@ testExpression = TestCase $ do
     conns <- Map.lookup 0 >>> fromJust >>> Xpr.getNeurConnections >>>
             readSTRef >>> stToIO $ org
     assertEqual "Did not express enabled connection" 1 $ Set.size conns
-    
 
-testMain = runTestTT testExpression
+-- |Build up a small genome and try to express it
+testExpressGenome = TestCase $ do
+    let nodeGenes = map Gene.NodeGene [0..4]
+    let connGenes = nodeGenes >>=
+                      \(Gene.NodeGene m) ->
+                        nodeGenes >>=
+                          \(Gene.NodeGene n) ->
+                            return (Gene.ConnectGene m n 1.0 True (m * 5 + n))
+
+    -- Express the organism
+    org <- stToIO $ Xpr.expressGenome (Gene.Genome nodeGenes connGenes) Map.empty
+
+    -- Test that all neurons have 5 connections
+    forM_ (Map.assocs org) $ \(id, neuron) -> do
+        conns <- stToIO $ readSTRef $ Xpr.getNeurConnections neuron
+        let message = "Neuron " ++ show id ++ " has wrong number of connections"
+        assertEqual message 5 $ Set.size conns
+
+testMain = runTestTT $ TestList [testSimpleExpress, testExpressGenome]
