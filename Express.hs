ecd380074b5b1c2cfc6c3e52232d7be28f980497
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-04-29 22:07:02 -0600

    Add stepOrganism

--- Express.hs
+++ Express.hs
@@ -9,6 +9,7 @@ import qualified Data.Set as Set
 
 import Control.Arrow ((***))
 import Control.Monad.ST
+import Data.List (sort)
 import Data.STRef
 
 data Connection s = Connection (Neuron s) Double
@@ -31,8 +32,12 @@ data Neuron s = Neuron {
     neurOutput :: STRef s Double -- |Output level
 }
 
+-- |An equality instance that just compares the neurons' IDs
 instance Eq (Neuron s) where
     n1 == n2 = (neurID n1) == (neurID n2)
+
+instance Ord (Neuron s) where
+    compare n1 n2 = compare (neurID n1) (neurID n2)
 
 -- |Mapping from IDs to neurons
 type Organism s = Map.Map Integer (Neuron s)
@@ -75,4 +80,35 @@ expressGenome (Genome nodeGenes connGenes) org = do
     return org'
 
 stepOrganism :: Organism s -> [Double] -> ST s [Double]
-stepOrganism org = error "unimp: stepOrganism"
+stepOrganism org invals = do
+    let nodes = sort . map snd . Map.toList $ org
+    let inputs = filter ((==) NodeIn . neurType) nodes
+    let outputs = filter ((==) NodeOut . neurType) nodes
+
+    -- Save the output values
+    outvals <- mapM (readSTRef . neurOutput) outputs
+
+    -- Initialize each node's inputs to zero
+    forM_ nodes $ \neuron ->
+      writeSTRef (neurInput neuron) 0.0
+
+    -- Add in the inputs from the outside world
+    forM_ (zip inputs invals) $ \(neuron, val) ->
+      modifySTRef (neurInput neuron) (+ val)
+
+    -- Propagate values forward in the network
+    forM_ nodes $ \neuron -> do
+      output_level <- readSTRef . neurOutput $ neuron
+      conns <- readSTRef . neurConns $ neuron
+      -- For each node, iterate over the nodes it feeds to
+      forM_ (Set.toList conns) $ \(Connection other weight) ->
+        modifySTRef (neurInput other) $ (+) (output_level * weight)
+
+    -- Fire all of the neurons (set their output values to the transfer
+    -- function applied to their input values)
+    forM_ nodes $ \neuron -> do
+      inval <- readSTRef . neurInput $ neuron
+      let outval = (neurTransfer neuron) inval
+      writeSTRef (neurOutput neuron) $ outval
+
+    return outvals
